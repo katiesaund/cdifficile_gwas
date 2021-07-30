5b789b27ccef52b384172ff536c2465106e2e539
Katie Saund <katiephd@umich.edu>
2021-07-30 11:18:35 -0400

    Add notebook

---
+++ notebook/run_markdown_files.sh
@@ -0,0 +1,9 @@
+Rscript -e "rmarkdown::render('CFE_GWAS.Rmd', output_file='CFE_GWAS.html')"
+Rscript -e "rmarkdown::render('FqR_GWAS.Rmd', output_file='FqR_GWAS.html')"
+Rscript -e "rmarkdown::render('Germ_Tc_and_Gly_GWAS.Rmd', output_file='Germ_Tc_and_Gly_GWAS.html')"
+Rscript -e "rmarkdown::render('Germ_Tc_GWAS.Rmd', output_file='Germ_Tc_GWAS.html')"
+Rscript -e "rmarkdown::render('Growth_GWAS.Rmd', output_file='Growth_GWAS.html')"
+Rscript -e "rmarkdown::render('Severity_GWAS.Rmd', output_file='Severity_GWAS.html')"
+Rscript -e "rmarkdown::render('Sporulation_GWAS.Rmd', output_file='Sporulation_GWAS.html')"
+Rscript -e "rmarkdown::render('Toxin_GWAS.Rmd', output_file='Toxin_GWAS.html')"
+
