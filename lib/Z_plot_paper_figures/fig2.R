2832a79daba275d2ed4c42d87a47f8c43495af7c
katiesaund <katiemsaund@gmail.com>
2022-02-28 21:07:21 -0800

    fix names and remove BM and WN

--- lib/Z_plot_paper_figures/fig2.R
+++ lib/Z_plot_paper_figures/fig2.R
@@ -31,11 +31,22 @@ phylo_signal_w_neg_ctrl_df <- phylo_signal_w_neg_ctrl_df %>%
 phylo_signal_w_neg_ctrl_df$negative_ctrl[phylo_signal_w_neg_ctrl_df$negative_ctrl == TRUE] <- "Negative control"
 phylo_signal_w_neg_ctrl_df$negative_ctrl[phylo_signal_w_neg_ctrl_df$negative_ctrl == FALSE] <- "Observed value"
 
+phylo_signal_w_neg_ctrl_df <- 
+  phylo_signal_w_neg_ctrl_df %>% 
+  mutate(plot_name = case_when(pretty_name == "germination in Tc" ~ "Germ. in TCA", 
+                               pretty_name == "germination in Tc&Gly" ~ "Germ. in TCA&Gly", 
+                               pretty_name == "fqR" ~ "Fluoroquinolone Resistance", 
+                               pretty_name == "spore viability" ~ "Spore viability", 
+                               pretty_name == "growth rate" ~ "Max growth rate", 
+                               pretty_name == "# spores" ~ "Total spores",
+                               pretty_name == "toxin activity" ~ "Cytotoxicity", 
+                               pretty_name == "Severity" ~ "Severe infection"))
+
 # Want to change plot order to: 
 # 1. germ tc
 # 2. germ TG
 # 3. growth
-# 4. # sportes
+# 4. # spores
 # 5. spore viability
 # 6. toxin 
 phylo_signal_w_neg_ctrl_df$plot_order[phylo_signal_w_neg_ctrl_df$pretty_name == "germination in Tc"] <- 1
@@ -48,7 +59,7 @@ phylo_signal_w_neg_ctrl_df$plot_order[phylo_signal_w_neg_ctrl_df$pretty_name == 
 set.seed(6) # to get a reproducible jitter distribution where all observed values aren't covered by negative controls
 dot_plot_wo_leg <- phylo_signal_w_neg_ctrl_df %>% 
   filter(bin_cont == "continuous") %>% 
-  ggplot(aes(x = fct_reorder(pretty_name, plot_order), y = value, col = negative_ctrl)) + 
+  ggplot(aes(x = fct_reorder(plot_name, plot_order), y = value, col = negative_ctrl)) + 
   geom_jitter(width = 0.25, height = 0) + 
   theme_bw() + 
   xlab("") + 
@@ -58,30 +69,6 @@ dot_plot_wo_leg <- phylo_signal_w_neg_ctrl_df %>%
   ylim(c(0, 1.0)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   labs(tag = "A")
-
-# some toy data
-df <- data.frame(x = 3, y = 0:1)
-# interpolate values from zero to y and create corresponding number of x values
-vals <- lapply(df$y, function(y) seq(0, y, by = 0.01))
-y <- unlist(vals)
-mid <- rep(df$x, lengths(vals))
-df2 <- data.frame(x = mid - 0.4,
-                  xend = mid + 0.4,
-                  y = y,
-                  yend = y)
-
-wn_plot <- ggplot(data = df2, aes(x = x, xend = xend, y = y, yend = yend, color = y)) +
-  geom_segment(size = 2) +
-  ggtitle("   WN") + 
-  scale_color_gradient2(low = "burlywood4", mid = "burlywood2", high = "white", 
-                        midpoint = max(df2$y)/2, guide = "none")  + 
-  theme_void() 
-bm_plot <- ggplot(data = df2, aes(x = x, xend = xend, y = y, yend = yend, color = y)) +
-  geom_segment(size = 2) +
-  labs(title = "   BM", size = 0.05) + 
-  scale_color_gradient2(low = "white", mid = "burlywood2", high = "burlywood4", 
-                        midpoint = max(df2$y)/2, guide = "none")  + 
-  theme_void()
 
 # 2B
 best_results_df <- read_tsv("../../data/15_pyseer/wgs_model_results/best_model_results.tsv", col_names = TRUE)
@@ -97,12 +84,22 @@ best_results_df$plot_order[best_results_df$pretty_name == "# spores"] <- 4
 best_results_df$plot_order[best_results_df$pretty_name == "spore viability"] <- 5
 best_results_df$plot_order[best_results_df$pretty_name == "toxin activity"] <- 6
 
+best_results_df <- 
+  best_results_df %>% 
+  mutate(plot_name = case_when(pretty_name == "germination in Tc" ~ "Germ. in TCA", 
+                               pretty_name == "germination in Tc&Gly" ~ "Germ. in TCA&Gly", 
+                               pretty_name == "fqR" ~ "Fluoroquinolone Resistance", 
+                               pretty_name == "spore viability" ~ "Spore viability", 
+                               pretty_name == "growth rate" ~ "Max growth rate", 
+                               pretty_name == "# spores" ~ "Total spores",
+                               pretty_name == "toxin activity" ~ "Cytotoxicity", 
+                               pretty_name == "Severity" ~ "Severe infection"))
 
 set.seed(2) # to get a reproducible jitter distribution where all observed values aren't covered by negative controls
 pyseer_plot <- best_results_df %>% 
   filter(bin_cont == "continuous") %>% 
   ggplot(mapping = aes(y = Rsquared,
-                       x = fct_reorder(pretty_name, plot_order), 
+                       x = fct_reorder(plot_name, plot_order), 
                        col = negative_control)) + 
   geom_jitter(width = 0.25, height = 0) +  
   scale_color_manual(values = c("grey", "black")) + 
@@ -113,8 +110,8 @@ pyseer_plot <- best_results_df %>%
   labs(tag = "B") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-dot_plot_wo_leg + wn_plot + bm_plot + pyseer_plot + 
-  plot_layout(ncol = 4, widths = c(6, 1, 1, 6)) & 
+dot_plot_wo_leg + pyseer_plot + 
+  plot_layout(ncol = 2, widths = c(7, 7)) & 
   theme(plot.tag = element_text(face = "bold"))
 ggsave("../../figures/Fig2.png", width = 8, height = 4, units = "in")
 ggsave("../../figures/Fig2.pdf", width = 8, height = 4, units = "in")
